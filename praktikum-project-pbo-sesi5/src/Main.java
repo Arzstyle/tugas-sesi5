0c7dfe89446371695e68ec03e942fadba3992db8
Muhamad Akbar Rizky Saputra <148222478+Arzstyle@users.noreply.github.com>
2025-03-13 18:41:16 +0700

    update

--- praktikum-project-pbo-sesi5/src/Main.java
+++ praktikum-project-pbo-sesi5/src/Main.java
@@ -4,17 +4,16 @@ public class Main {
         System.out.println("Selamat datang di Aplikasi Rekening Tohir");
         System.out.println("=====================================\n");
 
-        // Membuat rekening tabungan
+
         Tabungan tabungan = new Tabungan("Akbar", "123456", 0, 5.0);
         tabungan.setor(200000);
         tabungan.tarik(100000);
         tabungan.tampilkanInfo();
         tabungan.hitungBunga(6);
 
-        // Membuat rekening giro
-        // Giro giro = new Giro("Akbar", "654321", 0, 500000);
-        // giro.setor(250000, true);
-        // giro.tarik(200000);
-        // giro.tampilkanInfo();
+        Giro giro = new Giro("Akbar", "654321", 0, 500000);
+        giro.setor(250000, true);
+        giro.tarik(200000);
+        giro.tampilkanInfo();
     }
 }
