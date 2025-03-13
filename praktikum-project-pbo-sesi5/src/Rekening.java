4315c3c7706715f0b4f2bc9a0c75a533f914bbf0
Muhamad Akbar Rizky Saputra <148222478+Arzstyle@users.noreply.github.com>
2025-03-13 23:16:13 +0700

    -

--- praktikum-project-pbo-sesi5/src/Rekening.java
+++ praktikum-project-pbo-sesi5/src/Rekening.java
@@ -11,7 +11,7 @@ public class Rekening {
 
     public void setor(double jumlah) {
         saldo += jumlah;
-        System.out.println("Setoran berhasil! Saldo sekarang: Rp" + saldo);
+        System.out.println("Setoran berhasil! Saldo sekarang: Rp: " + saldo);
     }
 
     // Overloading setor() untuk transfer
@@ -21,20 +21,20 @@ public class Rekening {
             if (jumlah > biayaAdmin) {
                 saldo += (jumlah - biayaAdmin); 
                 System.out.println("Transfer diterima dari rekening lain!");
-                System.out.println("Biaya admin Rp 2.500 dipotong. Saldo sekarang: Rp" + saldo);
+                System.out.println("Biaya admin Rp 2.500 dipotong. Saldo sekarang: Rp: " + saldo);
             } else {
                 System.out.println("Transfer gagal! Jumlah tidak mencukupi setelah dipotong biaya admin.");
             }
         } else {
             saldo += jumlah;
-            System.out.println("Transfer berhasil! Saldo sekarang: Rp" + saldo);
+            System.out.println("Transfer berhasil! Saldo sekarang: Rp: " + saldo);
         }
     }
 
     public void tarik(double jumlah) {
         if (jumlah <= saldo) {
             saldo -= jumlah;
-            System.out.println("Penarikan berhasil! Saldo sekarang: Rp" + saldo);
+            System.out.println("Penarikan berhasil! Saldo sekarang: Rp: " + saldo);
         } else {
             System.out.println("Penarikan gagal! Saldo tidak mencukupi.");
         }
@@ -44,6 +44,6 @@ public class Rekening {
         System.out.println("\n=== Informasi Rekening ===");
         System.out.println("Nama Pemilik  : " + namaPemilik);
         System.out.println("No. Rekening  : " + noRekening);
-        System.out.println("Saldo         : Rp" + saldo);
+        System.out.println("Saldo         : Rp: " + saldo);
     }
 }
